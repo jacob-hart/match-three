0ae144dbef7248e1139fb673dae5744d29f155e8
jacob-hart <jacobhart1227@gmail.com>
2018-11-23 23:31:24 -0700

    Added 2D array to Grid and added default scene

--- scripts/grid.gd
+++ scripts/grid.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 export (int) var width_in_blocks
 export (int) var height_in_blocks
+export (int) var x_start_position
+export (int) var y_start_position # TODO make these determined programmatically
+export (int) var offset
 
 var potential_blocks = [
 	preload("res://scenes/block_magenta.tscn"),
@@ -13,10 +16,21 @@ var potential_blocks = [
 	preload("res://scenes/block_violet.tscn"),
 ]
 
+var blocks = []
+
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
+	blocks = make_2D_array()
+	print(blocks)
 	pass
+
+func make_2D_array():
+	var array = []
+	for i in width_in_blocks:
+		array.append([])
+		for j in height_in_blocks:
+			array[i].append(null)
+	return array
+
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
