e2417c4d00201f36a8fe9220e77621c01a7e6051
jacob-hart <jacobhart1227@gmail.com>
2019-04-06 23:14:10 -0600

    Add audio manager

--- scripts/Grid.gd
+++ scripts/Grid.gd
@@ -149,7 +149,7 @@ func would_match_be_formed_at(row, column, block_color):
 
 
 func play_sound(sound_name):
-	get_node(sound_name +"_sound").playing = true
+	get_parent().get_node("audio_manager").play(sound_name)
 
 # Converts a grid coordinate to a screen pixel coordinate
 func grid_to_pixel(row, column):
