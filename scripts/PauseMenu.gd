cdf38935e62d9fbf3e0a9dd966cf2d45b42eea48
jacob-hart <jacobhart1227@gmail.com>
2019-05-14 13:27:04 -0600

    Add quit to menu in addition to quit to desktop

--- scripts/PauseMenu.gd
+++ scripts/PauseMenu.gd
@@ -1,13 +1,24 @@
 extends CanvasLayer
 
+func pause_tree():
+	print("paused")
+	get_tree().paused = true
+
+func unpause_tree():
+	get_tree().paused = false
+	print("unpaused")
+
 func _on_button_resume_pressed():
 	print("resumed")
 
-func _on_button_quit_pressed():
+func _on_button_quit_to_menu_pressed():
+	get_tree().change_scene("res://scenes/MainMenu.tscn")
+
+func _on_button_quit_to_desktop_pressed():
 	get_tree().quit()
 
 func _notification(what):
     if what == MainLoop.NOTIFICATION_WM_FOCUS_IN:
-        print("tabbed in")
+        unpause_tree()
     elif what == MainLoop.NOTIFICATION_WM_FOCUS_OUT:
-        print("tabbed out")
+        pause_tree()
