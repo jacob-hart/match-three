a38b06917ed3067af691fb57eb595fb315553067
jacob-hart <jacobhart1227@gmail.com>
2019-05-14 19:25:06 -0600

    Add functional pause menu hiding and unhiding

--- scripts/PauseMenu.gd
+++ scripts/PauseMenu.gd
@@ -2,11 +2,19 @@ extends CanvasLayer
 
 func pause_tree():
 	print("paused")
+	show()
 	get_tree().paused = true
 
 func unpause_tree():
 	get_tree().paused = false
 	print("unpaused")
+	hide()
+
+func hide():
+	get_node("MarginContainer").hide()
+
+func show():
+	get_node("MarginContainer").show()
 
 func _on_button_resume_pressed():
 	unpause_tree()
@@ -17,6 +25,9 @@ func _on_button_quit_to_menu_pressed():
 
 func _on_button_quit_to_desktop_pressed():
 	get_tree().quit()
+
+func _ready():
+	hide()
 
 func _notification(what):
     if what == MainLoop.NOTIFICATION_WM_FOCUS_OUT:
