3f522979518952548e5c59c71c89dc610d80feae
jacob-hart <jacobhart1227@gmail.com>
2019-05-23 16:06:57 -0400

    Update pop label fadeout

--- scripts/PopLabel.gd
+++ scripts/PopLabel.gd
@@ -2,7 +2,7 @@ extends Label
 
 export (Vector2) var pop_scale = Vector2(2.5, 2.5)
 export (float) var pop_time = 0.15
-export (float) var fade_out_time = 4.0
+export (float) var fade_out_time = 0.5
 
 onready var pop_tween = get_node("pop_tween")
 
@@ -13,5 +13,5 @@ func set_text(new_text):
 
 func pop():
 	pop_tween.interpolate_property(self, "rect_scale", self.rect_scale, pop_scale, pop_time, Tween.TRANS_QUINT, Tween.EASE_OUT)
-	pop_tween.interpolate_property(self, "rect_scale", self.rect_scale, Vector2(1.0, 1.0), fade_out_time, Tween.TRANS_QUINT, Tween.EASE_IN, pop_time)
+	pop_tween.interpolate_property(self, "rect_scale", pop_scale, Vector2(1.0, 1.0), fade_out_time, Tween.TRANS_QUINT, Tween.EASE_OUT, pop_time)
 	pop_tween.start()
