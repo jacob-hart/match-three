8ea95d1151f2103664d0b93f80ba87c857fac189
jacob-hart <jacobhart1227@gmail.com>
2019-05-24 15:10:57 -0400

    Add variable PopLabel pop() scale

--- scripts/PopLabel.gd
+++ scripts/PopLabel.gd
@@ -1,6 +1,5 @@
 extends Label
 
-export (Vector2) var pop_scale = Vector2(3.0, 3.0)
 export (float) var pop_time = 0.15
 export (float) var fade_out_time = 0.5
 
@@ -11,7 +10,7 @@ func set_text(new_text):
 	self.rect_pivot_offset.x = self.rect_size.x / 2.0
 	self.rect_pivot_offset.y = self.rect_size.y / 2.0
 
-func pop():
-	pop_tween.interpolate_property(self, "rect_scale", self.rect_scale, pop_scale, pop_time, Tween.TRANS_QUINT, Tween.EASE_OUT)
+func pop(pop_scale = Vector2(3.0, 3.0)):
+	pop_tween.interpolate_property(self, "rect_scale", null, pop_scale, pop_time, Tween.TRANS_QUINT, Tween.EASE_OUT)
 	pop_tween.interpolate_property(self, "rect_scale", pop_scale, Vector2(1.0, 1.0), fade_out_time, Tween.TRANS_QUINT, Tween.EASE_OUT, pop_time)
 	pop_tween.start()
