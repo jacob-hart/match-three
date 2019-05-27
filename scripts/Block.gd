d7fa2de29c98a2ed08b17f72a0018d657b3de510
jacob-hart <jacobhart1227@gmail.com>
2019-05-27 16:14:33 -0400

    Update and reimplement selection in preparation for new effect

--- scripts/Block.gd
+++ scripts/Block.gd
@@ -11,7 +11,6 @@ export (float) var move_speed
 export (float) var select_speed
 export (float) var destroy_speed
 export (float) var destroy_fade_delay
-export (Vector2) var pressed_scale
 export (Vector2) var selected_scale 
 
 onready var move_tween = get_node("move_tween")
@@ -20,7 +19,6 @@ onready var select_tween = get_node("select_tween")
 
 onready var destroy_tween = get_node("destroy_tween")
 
-# Tweens the block to the target pixel position
 func move_smooth(position_to_move_to):
 	move_tween.interpolate_property(self, "position", null, position_to_move_to, move_speed, Tween.TRANS_QUINT, Tween.EASE_OUT)
 	move_tween.start()
@@ -35,17 +33,10 @@ func play_destroy_animation():
 	destroy_tween.interpolate_property(self, "modulate", null, Color(1.0, 1.0, 1.0, 0.0), destroy_speed - destroy_fade_delay, Tween.TRANS_QUINT, Tween.EASE_OUT, destroy_fade_delay)
 	destroy_tween.start()
 
-# This is called when the block is selected but the user has not released the mouse button on it yet
-func on_selected_pressed():
-	select_tween.interpolate_property(self, "scale", null, pressed_scale, select_speed, Tween.TRANS_QUINT, Tween.EASE_OUT)
-	select_tween.start()
-
-# This is called when the block is selected band the user has released the moused button
-func on_selected_released():
+func on_selected():
 	select_tween.interpolate_property(self, "scale", null, selected_scale, select_speed, Tween.TRANS_QUINT, Tween.EASE_OUT)
 	select_tween.start()
 
-# Resets any effects from selection back to normal
 func on_unselected():
 	select_tween.interpolate_property(self, "scale", null, Vector2(1.0, 1.0), select_speed, Tween.TRANS_QUINT, Tween.EASE_OUT)
 	select_tween.start()
