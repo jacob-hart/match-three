6e1ced9c77bd6283b21c7e7a657047347ac6c93c
jacob-hart <jacobhart1227@gmail.com>
2019-05-28 22:32:18 -0400

    Add basic config file saving and loading

--- scripts/GameModeScore.gd
+++ scripts/GameModeScore.gd
@@ -1,14 +1,24 @@
 extends "res://scripts/GameMode.gd"
 
 export (int) var starting_score = 0
+export (int) var default_high_score = 1000
 export (NodePath) var ui_score_label_path
+export (NodePath) var ui_high_score_label_path
 export (float) var match_size_weighting = 1.0
 export (float) var chain_count_weighting = 1.0
 export (int) var base_score_for_match = 100
 
 onready var ui_score_label = get_node(ui_score_label_path)
+onready var ui_high_score_label = get_node(ui_high_score_label_path)
 
-var score : int = 0
+onready var score:int = starting_score
+var high_score:int = default_high_score
+
+func _ready():
+	ui_high_score_label.set_text(String(high_score))
+
+func _process(delta):
+	ui_score_label.set_text(String(score))
 
 func on_game_over():
 	print("FINAL SCORE: ", score)
@@ -17,6 +27,6 @@ func on_game_over():
 func add_matched_block(match_size, chain_count, custom_weighting = 1.0):
 	score += ((match_size * match_size_weighting) * (chain_count * chain_count_weighting)) * custom_weighting * base_score_for_match
 	ui_score_label.pop()
-
-func _process(delta):
-	ui_score_label.set_text(String(score))
+	if score > high_score:
+		ui_high_score_label.set_text(String(score))
+		ui_high_score_label.pop()
---
+++ scripts/SavedData.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var file = ConfigFile.new()
+
+func _init():
+	var result = file.load("user://SavedData.ini")
+	if result == OK:
+		print("Successfully loaded config file")
+	else:
+		print("Error loading config file: ", result)
+
+func get_value(section, key):
+	return file.get_value(section, key)
+
+func set_value(section, key, value):
+	file.set_value(section, key, value)
+	file.save("user://SavedData.ini")
