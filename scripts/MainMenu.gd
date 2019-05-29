7ec6553d438fef2ebea5678783f72857affbad66
jacob-hart <jacobhart1227@gmail.com>
2019-05-28 22:05:52 -0400

    Rename time game mode

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 func _on_button_play_time_pressed():
-	get_tree().change_scene("res://scenes/GameModeTimeAttack.tscn")
+	get_tree().change_scene("res://scenes/GameModeTime.tscn")
 
 func _on_button_play_score_pressed():
 	print("UNIMPLEMENTED")
