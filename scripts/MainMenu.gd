08ae330a4dd3a38a526948bd8af1e5ae2e991056
jacob-hart <jacobhart1227@gmail.com>
2019-06-03 12:32:01 -0400

    Update UI theming

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -3,8 +3,5 @@ extends CanvasLayer
 func _on_button_play_time_pressed():
 	SceneChanger.change_scene("res://scenes/GameModeTime.tscn")
 
-func _on_button_play_score_pressed():
-	print("UNIMPLEMENTED")
-
 func _on_button_quit_pressed():
 	get_tree().quit()
