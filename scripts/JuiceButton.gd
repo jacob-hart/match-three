05521e6c9373091006a7ce6eba20f26e7ac06cb6
jacob-hart <jacobhart1227@gmail.com>
2019-06-06 21:24:32 -0400

    Refactor JuiceButton

--- scripts/JuiceButton.gd
+++ scripts/JuiceButton.gd
@@ -9,15 +9,21 @@ func _ready():
 	center_pivot()
 
 func _notification(what):
-	if what == Control.NOTIFICATION_MOUSE_ENTER:
-		tween.interpolate_property(self, "rect_scale", null, hover_scale, hover_time, Tween.TRANS_QUINT, Tween.EASE_OUT)
-		tween.start()
-	elif what == Control.NOTIFICATION_MOUSE_EXIT:
-		tween.interpolate_property(self, "rect_scale", null, Vector2(1.0, 1.0), hover_time, Tween.TRANS_QUINT, Tween.EASE_OUT)
-		tween.start()
+	if what == Control.NOTIFICATION_MOUSE_ENTER || what == Control.NOTIFICATION_FOCUS_ENTER:
+		expand(hover_scale, hover_time)
+	elif what == Control.NOTIFICATION_MOUSE_EXIT || what == Control.NOTIFICATION_FOCUS_EXIT:
+		return_to_normal_size()
 	elif what == Control.NOTIFICATION_RESIZED:
 		center_pivot()
 
 func center_pivot():
 	rect_pivot_offset.x = rect_size.x / 2
 	rect_pivot_offset.y = rect_size.y / 2
+
+func expand(scale, time):
+	tween.interpolate_property(self, "rect_scale", null, scale, time, Tween.TRANS_QUINT, Tween.EASE_OUT)
+	tween.start()
+
+func return_to_normal_size():
+	tween.interpolate_property(self, "rect_scale", null, Vector2(1.0, 1.0), hover_time, Tween.TRANS_QUINT, Tween.EASE_OUT)
+	tween.start()
