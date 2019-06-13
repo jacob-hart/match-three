7fa633f448d0e958f7556e7ef1a1f003f095cb43
jacob-hart <jacobhart1227@gmail.com>
2019-06-13 11:43:11 -0400

    Allow scene changing in non-debug releases

--- scripts/SceneChanger.gd
+++ scripts/SceneChanger.gd
@@ -17,7 +17,7 @@ func change_scene(path, transition_time = 0.2, delay = 0.0):
 	tween.start()
 	yield(tween, "tween_completed")
 	emit_signal("about_to_change_scene")
-	assert(get_tree().change_scene(path) == OK)
+	get_tree().change_scene(path)
 	tween.interpolate_property(shade, "color", null, Color(shade.color.r, shade.color.g, shade.color.b, 0.0), transition_time / 2.0, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	tween.start()
 	yield(tween, "tween_completed")
