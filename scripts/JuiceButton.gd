eb2841dd3a330e2ca05def6b22ed23e27e20961e
jacob-hart <jacobhart1227@gmail.com>
2019-06-26 23:35:26 -0400

    Increase JuiceButton juiciness

--- scripts/JuiceButton.gd
+++ scripts/JuiceButton.gd
@@ -1,7 +1,12 @@
 extends Button
 
+export (Vector2) var pressed_scale = Vector2(0.95, 0.95)
+export (float) var pressed_time = 0.1
+
 export (Vector2) var hover_scale = Vector2(1.05, 1.05)
 export (float) var hover_time = 0.1
+
+export (float) var return_to_normal_time = 0.25
 
 onready var tween = get_node("Tween")
 
@@ -10,21 +15,32 @@ func _ready():
 
 func _notification(what):
 	if what == Control.NOTIFICATION_MOUSE_ENTER || what == Control.NOTIFICATION_FOCUS_ENTER:
-		expand(hover_scale, hover_time)
-	elif what == Control.NOTIFICATION_MOUSE_EXIT || what == Control.NOTIFICATION_FOCUS_EXIT:
-		return_to_normal_size()
+		Audio.play("click", "UI")
 	elif what == Control.NOTIFICATION_RESIZED:
 		center_pivot()
+
+func _process(delta):
+	var draw_mode = get_draw_mode()
+	match draw_mode:
+		DRAW_NORMAL:
+			return_to_normal_size()
+		DRAW_PRESSED:
+			scale(pressed_scale, pressed_time)
+		DRAW_HOVER:
+			scale(hover_scale, hover_time)
 
 func center_pivot():
 	rect_pivot_offset.x = rect_size.x / 2
 	rect_pivot_offset.y = rect_size.y / 2
 
-func expand(scale, time):
-	Audio.play("click", "UI")
-	tween.interpolate_property(self, "rect_scale", null, scale, time, Tween.TRANS_QUINT, Tween.EASE_OUT)
-	tween.start()
+func scale(scale, time):
+	if self.rect_scale != scale:
+		tween.stop(self)
+		tween.interpolate_property(self, "rect_scale", null, scale, time, Tween.TRANS_QUINT, Tween.EASE_OUT)
+		tween.start()
 
 func return_to_normal_size():
-	tween.interpolate_property(self, "rect_scale", null, Vector2(1.0, 1.0), hover_time, Tween.TRANS_QUINT, Tween.EASE_OUT)
-	tween.start()
+	if self.rect_scale != Vector2(1.0, 1.0):
+		tween.stop(self)
+		tween.interpolate_property(self, "rect_scale", null, Vector2(1.0, 1.0), return_to_normal_time, Tween.TRANS_QUINT, Tween.EASE_OUT)
+		tween.start()
